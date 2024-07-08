cf8e1bfce67047248c8249c4e404d824f5f30613
Raheem Chisman <raheem.chisman@microsoft.com>
2024-07-07 19:46:46 -0700

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Raheem Chisman on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
